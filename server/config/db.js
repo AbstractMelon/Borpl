5c86f192ba800645e1fc83bd1c769e372a7d3d79
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-03-25 15:50:15 -0600

    Delete server directory

--- server/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/borpl', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-module.exports = db;
