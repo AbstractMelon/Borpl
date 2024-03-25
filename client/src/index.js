8e11ef6ac3385a55ee8f29f579d2aa94d2682f20
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-03-25 17:42:29 -0600

    Did frontend

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // You can create and import your own CSS file here
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
